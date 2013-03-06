d05ff105e35ffd04c22b43246facac888d624ab3
Jordi Ros <shine.3p@gmail.com>
2013-03-06 23:27:03 +0100

    Simple triangle drawing

--- apps/Hello World iOS/dryGL Hello World/HelloApp.mm
+++ apps/Hello World iOS/dryGL Hello World/HelloApp.mm
@@ -9,180 +9,36 @@
 #include "dry.h"
 #include "HelloApp.h"
 
+dry::ShaderBasic _shader;
+
 float _time = 0.f;
-
-GLuint _colorRenderBuffer;
-GLuint _positionSlot;
-GLuint _colorSlot;
-GLuint _projectionUniform;
-GLuint _modelViewUniform;
-float _currentRotation;
-GLuint _depthRenderBuffer;
-
-GLuint _floorTexture;
-GLuint _fishTexture;
-GLuint _texCoordSlot;
-GLuint _textureUniform;
-GLuint _vertexBuffer;
-GLuint _indexBuffer;
-GLuint _vertexBuffer2;
-GLuint _indexBuffer2;
-
-typedef struct {
-    float Position[3];
-    float Color[4];
-    float TexCoord[2];
-} Vertex;
-
-#define TEX_COORD_MAX   4
-
-const Vertex Vertices[] = {
-    // Front
-    {{1, -1, 0}, {1, 0, 0, 1}, {TEX_COORD_MAX, 0}},
-    {{1, 1, 0}, {0, 1, 0, 1}, {TEX_COORD_MAX, TEX_COORD_MAX}},
-    {{-1, 1, 0}, {0, 0, 1, 1}, {0, TEX_COORD_MAX}},
-    {{-1, -1, 0}, {0, 0, 0, 1}, {0, 0}},
-    // Back
-    {{1, 1, -2}, {1, 0, 0, 1}, {TEX_COORD_MAX, 0}},
-    {{-1, -1, -2}, {0, 1, 0, 1}, {TEX_COORD_MAX, TEX_COORD_MAX}},
-    {{1, -1, -2}, {0, 0, 1, 1}, {0, TEX_COORD_MAX}},
-    {{-1, 1, -2}, {0, 0, 0, 1}, {0, 0}},
-    // Left
-    {{-1, -1, 0}, {1, 0, 0, 1}, {TEX_COORD_MAX, 0}},
-    {{-1, 1, 0}, {0, 1, 0, 1}, {TEX_COORD_MAX, TEX_COORD_MAX}},
-    {{-1, 1, -2}, {0, 0, 1, 1}, {0, TEX_COORD_MAX}},
-    {{-1, -1, -2}, {0, 0, 0, 1}, {0, 0}},
-    // Right
-    {{1, -1, -2}, {1, 0, 0, 1}, {TEX_COORD_MAX, 0}},
-    {{1, 1, -2}, {0, 1, 0, 1}, {TEX_COORD_MAX, TEX_COORD_MAX}},
-    {{1, 1, 0}, {0, 0, 1, 1}, {0, TEX_COORD_MAX}},
-    {{1, -1, 0}, {0, 0, 0, 1}, {0, 0}},
-    // Top
-    {{1, 1, 0}, {1, 0, 0, 1}, {TEX_COORD_MAX, 0}},
-    {{1, 1, -2}, {0, 1, 0, 1}, {TEX_COORD_MAX, TEX_COORD_MAX}},
-    {{-1, 1, -2}, {0, 0, 1, 1}, {0, TEX_COORD_MAX}},
-    {{-1, 1, 0}, {0, 0, 0, 1}, {0, 0}},
-    // Bottom
-    {{1, -1, -2}, {1, 0, 0, 1}, {TEX_COORD_MAX, 0}},
-    {{1, -1, 0}, {0, 1, 0, 1}, {TEX_COORD_MAX, TEX_COORD_MAX}},
-    {{-1, -1, 0}, {0, 0, 1, 1}, {0, TEX_COORD_MAX}},
-    {{-1, -1, -2}, {0, 0, 0, 1}, {0, 0}}
-};
-
-const GLubyte Indices[] = {
-    // Front
-    0, 1, 2,
-    2, 3, 0,
-    // Back
-    4, 5, 6,
-    6, 7, 4,
-    // Left
-    8, 9, 10,
-    10, 11, 8,
-    // Right
-    12, 13, 14,
-    14, 15, 12,
-    // Top
-    16, 17, 18,
-    18, 19, 16,
-    // Bottom
-    20, 21, 22,
-    22, 23, 20
-};
-
-const Vertex Vertices2[] = {
-    {{0.5, -0.5, 0.01}, {1, 1, 1, 1}, {1, 1}},
-    {{0.5, 0.5, 0.01}, {1, 1, 1, 1}, {1, 0}},
-    {{-0.5, 0.5, 0.01}, {1, 1, 1, 1}, {0, 0}},
-    {{-0.5, -0.5, 0.01}, {1, 1, 1, 1}, {0, 1}},
-};
-
-const GLubyte Indices2[] = {
-    1, 0, 2, 3
-};
-
-
-
-void setupVBO()
-{
-    glGenBuffers(1, &_vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &_indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &_vertexBuffer2);
-    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer2);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices2), Vertices2, GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &_indexBuffer2);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer2);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices2), Indices2, GL_STATIC_DRAW);
-}
-
 
 void render()
 {
-    glClear(GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
-    
-    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
-
-    float h = 4.0f * 640 / 960;
-    glm::mat4 proj = glm::frustum(-2.f, 2.f, -h/2.f, h/2.f, 4.f, 10.f);
-    glUniformMatrix4fv(_projectionUniform, 1.f, 0.f, (GLfloat *)&proj);
-
-    glm::mat4 model = glm::translate(0.f, 0.f, -7.f);
-    _currentRotation += _time * 90.f;
-    //[modelView rotateBy:CC3VectorMake(_currentRotation, _currentRotation, 0)];
-    glUniformMatrix4fv(_modelViewUniform, 1.f, 0.f, (GLfloat *)&model);
-    
     // 1
-    glViewport(0, 0, 640,960);
+    glViewport(0,0, 640,960);
     
-    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
-    
-    // 2
-    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) (sizeof(float) * 3));
-    
-    glVertexAttribPointer(_texCoordSlot, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) (sizeof(float) * 7));
-    
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, _floorTexture);
-    glUniform1i(_textureUniform, 0);
-    
-    // 3
-    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer2);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer2);
-    
-    glActiveTexture(GL_TEXTURE0); // unneccc in practice
-    glBindTexture(GL_TEXTURE_2D, _fishTexture);
-    glUniform1i(_textureUniform, 0); // unnecc in practice
-    
-    glUniformMatrix4fv(_modelViewUniform, 1.f, 0.f, (GLfloat *)&model);
-    
-    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) (sizeof(float) * 3));
-    glVertexAttribPointer(_texCoordSlot, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) (sizeof(float) * 7));
-    
-    glDrawElements(GL_TRIANGLE_STRIP, sizeof(Indices2)/sizeof(Indices2[0]), GL_UNSIGNED_BYTE, 0);
-}
+    _shader.Begin();
 
-dry::ShaderBasic _shader;
+    static GLfloat vVertices[] = {0.0f, 0.5f, 0.0f,
+                            -0.5f, -0.5f, 0.0f,
+                            0.5f, -0.5f, 0.0f};
+    // Load the vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
+    glEnableVertexAttribArray(0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    _shader.End();
+}
 
 void HelloApp::Init()
 {
-    setupVBO();
     _shader.Init();
     int programHandle = _shader.GetShader()->GetHandleProgram();
-    // 5
+
+    glBindAttribLocation(programHandle, 0, "aPos");
+    
+    /*
     _positionSlot = glGetAttribLocation(programHandle, "aPos");
     _colorSlot = glGetAttribLocation(programHandle, "aColor");
     glEnableVertexAttribArray(_positionSlot);
@@ -194,7 +50,7 @@ void HelloApp::Init()
     _texCoordSlot = glGetAttribLocation(programHandle, "aTexCoord");
     glEnableVertexAttribArray(_texCoordSlot);
     _textureUniform = glGetUniformLocation(programHandle, "texture");
-
+    */
 }
 
 
@@ -208,7 +64,5 @@ void HelloApp::Draw()
     float r = fmod(_time, 1.f);
     glClearColor(0, r, 55.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    _shader.Begin();
     render();
-    _shader.End();
 }
--- src/graphics/ShaderBasic.cpp
+++ src/graphics/ShaderBasic.cpp
@@ -18,28 +18,13 @@ using namespace dry;
 //------------------------------------------------------------------------------------------------
 static const char *s_VS =
     "attribute vec4 aPos;"
-    "attribute vec4 aCol;"
-    "attribute vec2 aTexCoord;"
-
-    "uniform mat4 projection;"
-    "uniform mat4 modelView;"
-
-    "varying vec2 vTexCoord;"
-    "varying vec4 vColor;"
 
     "void main(void)"
     "{"
-        "vColor = aCol;"
-        "vTexCoord = aTexCoord;"
-        "gl_Position = projection * modelView * aPos;"
+        "gl_Position = aPos;"
     "}";
 
 static const char *s_FS =
-    "varying lowp vec4 vColor;"
-    "varying lowp vec2 vTexCoord;"
-
-    "uniform sampler2D texture;"
-
     "void main(void)"
     "{"
         "gl_FragColor = vec4(1.0,1.0,1.0,1.0);"
